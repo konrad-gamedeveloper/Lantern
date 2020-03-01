4fdb3ef1320c478cf2f4288b934fe2ae1077c777
itam14 <61597755+itam14@users.noreply.github.com>
2020-03-02 00:19:42 +0100

    add player movment
    Add: horizontal movment, jump, crouch + shrinking box collider.

---
+++ Assets/Mati/scripts/Crouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+    public BoxCollider2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = player.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            player.size = new Vector3 (1, 0.5f, 1);
+            player.offset = new Vector3(0, -0.25f, 0);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            player.size = new Vector3(1, 1, 1);
+            player.offset = new Vector3(0, 0, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            player.size = new Vector3(1, 1, 1);
+            player.offset = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Mati/scripts/Move2D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public bool crouch = false;
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movment * Time.deltaTime * moveSpeed;
+        if (Input.GetKeyDown("s") && crouch == false)
+        {
+
+            moveSpeed = moveSpeed / 2;
+            crouch = true;
+
+        }
+        if (Input.GetKeyDown("w") && crouch == true)
+        {
+            moveSpeed = moveSpeed * 2;
+            crouch = false;
+        }
+        if (Input.GetKeyDown("space") && crouch == true)
+        {
+            moveSpeed = moveSpeed * 2;
+            crouch = false;
+        }
+        void Jump()
+        {
+            if (Input.GetButtonDown("Jump") && isGrounded == true)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+
+            }
+        }
+
+    }
+}
