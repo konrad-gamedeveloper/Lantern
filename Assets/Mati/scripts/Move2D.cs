ade3b1b020cb40a95e729f809ec9ab8883f85197
itam14 <61597755+itam14@users.noreply.github.com>
2020-03-02 16:06:56 +0100

    poprawki movmeny

--- Assets/Mati/scripts/Move2D.cs
+++ Assets/Mati/scripts/Move2D.cs
@@ -6,6 +6,8 @@ public class Move2D : MonoBehaviour
 {
     public bool crouch = false;
     public float moveSpeed = 5f;
+    public float jumpSpeed = 5f;
+
     public bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
@@ -19,28 +21,32 @@ public class Move2D : MonoBehaviour
         Jump();
         Vector3 movment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movment * Time.deltaTime * moveSpeed;
+        //spowolnienie dla "s" i flaga kucania
         if (Input.GetKeyDown("s") && crouch == false)
         {
-
+    
             moveSpeed = moveSpeed / 2;
             crouch = true;
 
         }
+        //odblokowanie spowolnienia dla "w" i anulowanie flagi kucania
         if (Input.GetKeyDown("w") && crouch == true)
         {
             moveSpeed = moveSpeed * 2;
             crouch = false;
         }
+        //odblokowanie spowolnienia dla "space" anulowanie flagi kucania
         if (Input.GetKeyDown("space") && crouch == true)
         {
             moveSpeed = moveSpeed * 2;
             crouch = false;
         }
+        //funkcja skoku
         void Jump()
         {
             if (Input.GetButtonDown("Jump") && isGrounded == true)
             {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
 
             }
         }
