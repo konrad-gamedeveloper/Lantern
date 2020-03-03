c2b6ced834b3e9236fb3d997a0d7cd3713f8ed79
itam14 <61597755+itam14@users.noreply.github.com>
2020-03-03 19:53:48 +0100

    nowe sterowanie

---
+++ Assets/Mati/nowe sterowanie/PlayerInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// nadanie pierwszenstwa skryptowi żeby nie było laga przy naciskaniu klawiszy
+[DefaultExecutionOrder(-100)]
+public class PlayerInput : MonoBehaviour
+{
+    [HideInInspector] public float horizontal;      //Float przechowujący input horyzontalny
+    [HideInInspector] public bool jumpHeld;         //Bool przechowujący nacisnięcie skoku
+    [HideInInspector] public bool jumpPressed;      //Bool przechowujący trzymanie skoku
+    [HideInInspector] public bool crouchHeld;       //Bool przechowujący nacisnięcie kucania
+    [HideInInspector] public bool crouchPressed;    //Bool przechowujący trzymanie kucania
+
+    bool readyToClear;                              //Bool uzywany do synchronizacji inputów
+    
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //czyszczenie istniejącuch inputów
+        ClearInput();
+    
+        //przetwarzanie inputów
+        ProcessInputs();
+        
+        // ustawienie horyzontalnych inputów pomiędzy -1 i 1
+        horizontal = Mathf.Clamp(horizontal, -1f, 1f);
+    }
+
+    private void FixedUpdate()
+    {
+        readyToClear = true;
+    }
+    void ClearInput()
+    {
+        //jesi funkcja nie jest true, wyjdz
+        if (!readyToClear)
+            return;
+
+        //resetujemy wszystkie inputy
+        horizontal      = 0f;
+        jumpPressed     = false;
+        jumpHeld        = false;
+        crouchPressed   = false;
+        crouchHeld      = false;
+
+        readyToClear    = false;
+    }
+    void ProcessInputs()
+    {
+        //zbieranie inputów horyzontalnych
+        horizontal      += Input.GetAxis("Horizontal");
+
+        // zbieranie inputów skoku 
+        jumpPressed     = jumpPressed || Input.GetButtonDown("Jump");
+        jumpHeld        = jumpHeld || Input.GetButton("Jump");
+
+        // zbieranie inputów kucania
+        crouchPressed = crouchPressed || Input.GetButtonDown("Crouch");
+        crouchHeld      = crouchHeld || Input.GetButton("Crouch");
+    }
+    
+}
---
+++ Assets/Mati/nowe sterowanie/PlayerMovement.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool drawDebugRaycasts = true;
+
+    [Header("Movment Propertise")]
+    public float speed = 8f;
+    public float crouchSpeedDivisor = 3f;
+    public float coyoteDuration = .05f;
+    public float maxFallSpeed = -25f;
+
+    [Header("Jump Properties")]
+    public float jumpForce = 6.3f;
+    public float crouchJumpBoost = 2.5f;
+    public float jumpHoldForce = 1.9f;
+    public float jumpHoldDuration = .1f;
+
+    [Header("Enviroment Check Properties")]
+    public float footOffset = .4f;
+    public float headClearance = .5f;
+    public float groundDistance = .2f;
+    public LayerMask groundLayer;
+
+    [Header("Status Flags")]
+    public bool isOnGround;
+    public bool isJumping;
+    public bool isCrouching;
+    public bool isHeadBlocked;
+
+    PlayerInput input;
+    BoxCollider2D bodyCollider;
+    Rigidbody2D rigidBody;
+
+    float jumpTime;
+    float coyoteTime;
+    float playerHeight;
+
+    float originalXScale;
+    int direction = 1;
+
+    Vector2 colliderStandSize;
+    Vector2 colliderStandOffset;
+    Vector2 colliderCrouchSize;
+    Vector2 colliderCrouchOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        bodyCollider = GetComponent<BoxCollider2D>();
+
+        originalXScale = transform.localScale.x;
+
+        playerHeight = bodyCollider.size.y;
+
+        colliderStandSize = bodyCollider.size;
+        colliderStandOffset = bodyCollider.offset;
+
+        colliderCrouchSize = new Vector2(bodyCollider.size.x, bodyCollider.size.y / 2f);
+        colliderStandOffset = new Vector2(bodyCollider.offset.x, bodyCollider.offset.y / 2f);
+
+    }
+
+    void FixedUpdate()
+    {
+        PhysicsCheck();
+
+        GroundMovement();
+        MidAirMovement();
+    }
+
+    void PhysicsCheck()
+    {
+        isOnGround = false;
+        isHeadBlocked = false;
+
+        RaycastHit2D leftcheck = Raycast(new Vector2(-footOffset, -0.5f), Vector2.down, groundDistance);
+        RaycastHit2D rightCheck = Raycast(new Vector2(footOffset, -0.5f), Vector2.down, groundDistance);
+
+
+        if (leftcheck || rightCheck)
+            isOnGround = true;
+
+        RaycastHit2D headCheck = Raycast(new Vector2(0f, 0.2f), Vector2.up, headClearance);
+
+        if (headCheck)
+            isHeadBlocked = true;
+
+    }
+
+    void GroundMovement()
+    {
+        if (input.crouchHeld && !isCrouching && isOnGround)
+            Crouch();
+        else if (!input.crouchHeld && isCrouching)
+            StandUp();
+        else if (!isOnGround && isCrouching)
+            StandUp();
+
+        float xVelocity = speed * input.horizontal;
+
+        if (xVelocity * direction < 0f)
+            FlipCharacterDirection();
+
+        if (isCrouching)
+            xVelocity /= crouchSpeedDivisor;
+
+        rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
+
+        if (isOnGround)
+            coyoteTime = Time.time + coyoteDuration;
+    }
+
+    void MidAirMovement()
+    {
+
+    
+    // Update is called once per frame
+    
+    
+        if (input.jumpPressed && !isJumping && (isOnGround || coyoteTime > Time.time))
+        {
+            if (isCrouching && !isHeadBlocked)
+            {
+                StandUp();
+                rigidBody.AddForce(new Vector2(0f, crouchJumpBoost), ForceMode2D.Impulse);
+            }
+
+            isOnGround = false;
+            isJumping = true;
+
+            jumpTime = Time.time + jumpHoldDuration;
+
+            rigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+        }
+        else if (isJumping)
+        {
+            if (input.jumpHeld)
+                rigidBody.AddForce(new Vector2(0f, jumpHoldForce), ForceMode2D.Impulse);
+
+            if (jumpTime <= Time.time)
+                isJumping = false;
+        }
+        if (rigidBody.velocity.y < maxFallSpeed)
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, maxFallSpeed);
+    }
+
+    void FlipCharacterDirection()
+    {
+        direction *= -1;
+
+        Vector3 scale = transform.localScale;
+
+        scale.x = originalXScale * direction;
+
+        transform.localScale = scale;
+
+    }
+
+    void Crouch()
+    {
+
+        isCrouching = true;
+
+        bodyCollider.size = colliderCrouchSize;
+        bodyCollider.offset = colliderCrouchOffset;
+
+    }
+
+    void StandUp()
+    {
+        if (isHeadBlocked)
+            return;
+
+        isCrouching = false;
+
+        bodyCollider.size = colliderStandSize;
+        bodyCollider.offset = colliderStandOffset;
+    }
+    RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length)
+    {
+        return Raycast(offset, rayDirection, length, groundLayer);
+    }
+    RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask mask)
+    {
+        Vector2 pos = transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDirection, length, mask);
+
+        if (drawDebugRaycasts)
+        {
+            //...determine the color based on if the raycast hit...
+            Color color = hit ? Color.red : Color.green;
+            //...and draw the ray in the scene view
+            Debug.DrawRay(pos + offset, rayDirection * length, color);
+        }
+
+        return hit;
+    }
+}
